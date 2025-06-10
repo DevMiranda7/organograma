e9734915850cc6a4eb9c4962e73543157b5c04fb
Nemoske <luandevfreitas7@gmail.com>
2025-06-09 21:49:28 -0300

    Inicio do projeto

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Banner from "./componentes/Banner/banner";
 
 function App() {
   return (
     <div className="App">
+      <Banner />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Hello, World!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/componentes/Banner/banner.js
@@ -0,0 +1,11 @@
+import "./banner.css";
+function Banner() {
+  // JSX
+  return (
+    <header className="banner">
+      <img src="/imagens/banner.png" alt="banner principal da página"></img>
+    </header>
+  );
+}
+
+export default Banner;
