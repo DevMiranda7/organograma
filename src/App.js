33ad3d259c85d5b1173ef93cb7b908a8c11d93af
Nemoske <luandevfreitas7@gmail.com>
2025-06-10 23:01:05 -0300

    Inputs add

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from "./logo.svg";
-import "./App.css";
-import Banner from "./componentes/Banner/banner";
+import Banner from "./componentes/Banner";
+import CampoTexto from "./componentes/CampoTexto";
 
 function App() {
   return (
     <div className="App">
       <Banner />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello, World!</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CampoTexto label="Nome" placeholder="Digite seu nome" />
+      <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
+      <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem" />
     </div>
   );
 }
